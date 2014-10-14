bfc4bc0ec4a1b979445b156405740d6ee036bbc9
Rami Sayar <rami.sayar@gmail.com>
2014-10-14 11:42:41 -0700

    Added FileIO Demo 3

---
+++ 03_FileIO/app.js
@@ -0,0 +1,8 @@
+﻿var fs = require('fs');
+
+var contents = fs.readFileSync('package.json').toString();
+console.log(contents);
+
+fs.readFile('package.json', function (err, buf) {
+    console.log(buf.toString());
+});
