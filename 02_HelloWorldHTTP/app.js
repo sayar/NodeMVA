8f6a76b504806bb34fde9c6e0c8a7f018ae274fb
Rami Sayar <rami.sayar@gmail.com>
2014-10-14 11:40:43 -0700

    Added HelloWorldHTTP Demo 2

---
+++ 02_HelloWorldHTTP/app.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World\n");
+});
+
+server.listen(7000);
