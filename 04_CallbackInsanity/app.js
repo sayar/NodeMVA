c77272c8396772e619d7b7bdb7c241ca068acb2d
Rami Sayar <rami.sayar@gmail.com>
2014-10-14 12:09:21 -0700

    Added CallbackInsanity Demo 4

---
+++ 04_CallbackInsanity/app.js
@@ -0,0 +1,17 @@
+﻿var fs = require('fs');
+
+fs.readdir('.', function (err, files) {
+    if (err) {
+        console.log('Error finding files: ' + err)
+    } else {
+        files.forEach(function (filename, fileIndex) {
+            fs.readFile(filename, function (err, buf) {
+                if (err) {
+                    console.log('Error reading file:' + err);   
+                } else {
+                    console.log(buf.toString());
+                }
+            });
+        });
+    }
+});
