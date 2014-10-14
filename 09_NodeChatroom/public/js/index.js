bb9f9f398059c8add1ac9d1638c759a90014d956
Rami Sayar <rami.sayar@gmail.com>
2014-10-14 12:47:02 -0700

    Added Node Chatroom Demo 9

---
+++ 09_NodeChatroom/public/js/index.js
@@ -0,0 +1,11 @@
+﻿var socket = io();
+$('#send-message-btn').click(function () {
+    var msg = $('#message-box').val();
+    socket.emit('chat', msg);
+    $('#messages').append($('<p>').text(msg));
+    $('#message-box').val('');
+    return false;
+});
+socket.on('chat', function (msg) {
+    $('#messages').append($('<p>').text(msg));
+});
