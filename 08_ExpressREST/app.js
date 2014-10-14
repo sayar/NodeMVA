2f391ff1ab98e5c73d2750f515c5b811b578d3ed
Rami Sayar <rami.sayar@gmail.com>
2014-10-14 12:36:33 -0700

    Added Express REST Demo 8

---
+++ 08_ExpressREST/app.js
@@ -0,0 +1,8 @@
+﻿var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.listen(process.env.PORT || 8080);
