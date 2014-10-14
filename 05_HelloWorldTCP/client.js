172d7bfc742590d565ad5a25ce7c63e3bb518042
Rami Sayar <rami.sayar@gmail.com>
2014-10-14 12:29:05 -0700

    Added HelloWorldTCP Demo 5

---
+++ 05_HelloWorldTCP/client.js
@@ -0,0 +1,15 @@
+﻿var net = require('net');
+
+var client = new net.Socket();
+
+client.connect(7000, "127.0.0.1");
+
+client.on('data', function (data) {
+    console.log('Data: ' + data);
+    client.destroy();
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function () {
+    console.log('Connection closed');
+});
---
+++ 05_HelloWorldTCP/server.js
@@ -0,0 +1,9 @@
+﻿var net = require('net');
+
+// The handler argument is automatically set as a listener for the 'connection' event
+var server = net.createServer(function (socket) {
+    console.log("Connection from " + socket.remoteAddress);
+    socket.end("Hello World\n");
+});
+
+server.listen(7000, "127.0.0.1");
