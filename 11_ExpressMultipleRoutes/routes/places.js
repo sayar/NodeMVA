61cb077a26dc86c8ada0bd0deebadc8841c968e4
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2015-07-29 09:48:23 -0400

    Jade templates
    Added in a few elements

--- 11_ExpressMultipleRoutes/routes/places.js
+++ 11_ExpressMultipleRoutes/routes/places.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('places', { title: 'Places I Plan on Visiting', 
+  res.render('places', { title: 'Places I Plan on Visiting',hasPlaces:true,
     places:{1:{name:"Japan",url:"http://en.wikipedia.org/wiki/Japan"},
             2:{name:"Scotland",url:"http://en.wikipedia.org/wiki/Scotland"},
             3:{name:"Brazil",url:"http://en.wikipedia.org/wiki/Brazil"}}});
