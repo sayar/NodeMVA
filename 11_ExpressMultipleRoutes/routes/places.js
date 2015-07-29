03aca1417e19f558e688a6b6b712bce5c0054b40
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2015-07-29 09:35:36 -0400

    Added in a few jade things
    and a launch json

--- 11_ExpressMultipleRoutes/routes/places.js
+++ 11_ExpressMultipleRoutes/routes/places.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('places', { title: 'Places I Plan on Visiting', places:{1:{name:"Japan",url:"http://www.google.com"}}});
+  res.render('places', { title: 'Places I Plan on Visiting', 
+    places:{1:{name:"Japan",url:"http://en.wikipedia.org/wiki/Japan"},
+            2:{name:"Scotland",url:"http://en.wikipedia.org/wiki/Scotland"},
+            3:{name:"Brazil",url:"http://en.wikipedia.org/wiki/Brazil"}}});
 });
 
 module.exports = router;
