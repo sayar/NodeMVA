e6c3145d1dc5f18cc5934c9ca3eb758101b2d5d4
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2015-07-29 10:22:54 -0400

    Changed names of folders
    added in a more robust requests bit

--- 10_Requests/app.js
+++ 10_Requests/app.js
@@ -1,5 +1,25 @@
 var request = require("request");
- 
+var fs = require("fs");
+//Example 1 : Let's grab a page 
 request("http://www.bing.com", function(error, response, body) {
-  console.log(body);
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+
 });
+
+//Example 2 : Simple Download of a file , kinda with no callback
+/*
+request('http://i.ytimg.com/vi/HrnBrOytj-A/0.jpg').pipe(fs.createWriteStream('rami.jpg'));
+*/
+
+/*
+// Example 3: We can download a file, and capture when its done withe finish event
+var file = fs.createWriteStream('rami.jpg');
+request('http://i.ytimg.com/vi/HrnBrOytj-A/0.jpg').pipe(file);
+file.on('finish',function(){
+  console.log("Okay, we got this stellar pic of Rami");
+})
+
+*/
+
---
+++ 11_ExpressMultiple/routes/places.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+   res.render('index', { title: 'Places' });
+});
+
+module.exports = router;
