021c67c649c3541c1273751f4c0568f90548ca1c
Progyan Bhattacharya <bprogyan@gmail.com>
2016-08-01 16:54:39 +0530

    Update app.js

--- 07_BasicExpress/app.js
+++ 07_BasicExpress/app.js
@@ -4,6 +4,8 @@
  */
 
 var express = require('express');
+var morgan  require('morgan');
+var json = require('json3');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
@@ -15,19 +17,21 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(express.favicon());
+app.use(morgan('dev'));
+app.use(json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
+*/
 
 app.get('/', routes.index);
 app.get('/users', user.list);
